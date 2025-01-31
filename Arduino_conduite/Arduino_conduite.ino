297ef783172d36d255311acadd733d1f02306d96
sideking <jeremy-roy-cote@hotmail.com>
2025-01-31 14:55:32 -0500

    debut code arduino

---
+++ Arduino_conduite/Arduino_conduite.ino
@@ -0,0 +1,51 @@
+#include <M5Atom.h>
+
+CRGB pixel;
+
+unsigned long monChronoMessages;
+
+#include <MicroOscSlip.h>
+MicroOscSlip<128> monOsc(&Serial);
+
+#include <M5_PbHub.h>
+M5_PbHub myPbHub;
+
+void setup() {
+  // put your setup code here, to run once:
+  M5.begin(false, false, false);
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(&pixel, 1);  // Ajouter le pixel du M5Atom à FastLED
+  Serial.begin(115200);
+
+  unsigned long chronoDepart = millis();
+  while (millis() - chronoDepart < 5000) {
+    pixel = CRGB(255, 255, 255);
+    FastLED.show();
+    delay(100);
+
+    pixel = CRGB(0, 0, 0);
+    FastLED.show();
+    delay(100);
+  }
+
+  pixel = CRGB(0, 0, 0);
+  FastLED.show();
+
+  Wire.begin();
+  myPbHub.begin();
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  M5.update();
+
+  //monOsc.onOscMessageReceived(maReceptionMessageOsc);
+
+  // À CHAQUE 20 MS I.E. 50x PAR SECONDE
+  if (millis() - monChronoMessages >= 20) {
+    monChronoMessages = millis();
+
+    //monOsc.sendInt("/rotation", encoderRotation);
+
+  }
+}
