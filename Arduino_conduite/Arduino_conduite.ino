9716f483faec84d04a2f0fe8534357920360d9cf
vince1887 <ian.corbin.qc@gmail.com>
2025-02-12 17:09:52 -0500

    RAW

--- Arduino_conduite/Arduino_conduite.ino
+++ Arduino_conduite/Arduino_conduite.ino
@@ -16,6 +16,17 @@ MicroOscSlip<128> monOsc(&Serial);
 #include <M5_PbHub.h>
 M5_PbHub myPbHub;
 
+#include "M5_ADS1115.h"
+ 
+#define M5_UNIT_VMETER_I2C_ADDR             0x49
+#define M5_UNIT_VMETER_EEPROM_I2C_ADDR      0x53
+#define M5_UNIT_VMETER_PRESSURE_COEFFICIENT 0.015918958F
+ 
+ADS1115 Vmeter;
+ 
+float resolution         = 0.0;
+float calibration_factor = 0.0;
+
 void setup() {
   // put your setup code here, to run once:
   M5.begin(false, false, false);
@@ -44,15 +55,31 @@ void setup() {
   myPbHub.setPixelColor(BUTTON_AFFIRM, 0, 0, 255, 0);
   myPbHub.setPixelColor(BUTTON_TEASE, 0, 255, 0, 0);
 
+  while (!Vmeter.begin(&Wire, M5_UNIT_VMETER_I2C_ADDR, 26, 32, 400000U)) {
+    Serial.println("Unit Vmeter Init Fail");
+    delay(1000);
+  }
+  Vmeter.setEEPROMAddr(M5_UNIT_VMETER_EEPROM_I2C_ADDR);
+  Vmeter.setMode(ADS1115_MODE_SINGLESHOT);
+  Vmeter.setRate(ADS1115_RATE_8);
+  Vmeter.setGain(ADS1115_PGA_512);
+  // | PGA      | Max Input Voltage(V) |
+  // | PGA_6144 |        128           |
+  // | PGA_4096 |        64            |
+  // | PGA_2048 |        32            |
+  // | PGA_512  |        16            |
+  // | PGA_256  |        8             |
+
+  resolution = Vmeter.getCoefficient() / M5_UNIT_VMETER_PRESSURE_COEFFICIENT;
+  calibration_factor = Vmeter.getFactoryCalibration();
+
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
   M5.update();
-
   //monOsc.onOscMessageReceived(maReceptionMessageOsc);
-
   // À CHAQUE 20 MS I.E. 50x PAR SECONDE
   if (millis() - monChronoMessages >= 20) {
     monChronoMessages = millis();
@@ -77,6 +104,14 @@ void loop() {
       }
     }
     lastTease = TeaseKey;
+
+    int16_t adc_raw = Vmeter.getSingleConversion();
+    float voltage   = adc_raw * resolution * calibration_factor;
+    /*Serial.printf("Cal ADC:%.0f\n", adc_raw * calibration_factor);
+    Serial.printf("Cal Voltage:%.2f mV\n", voltage);
+    Serial.printf("Raw ADC:%d\n\n", adc_raw);*/
+
+    monOsc.sendFloat("/Raw", adc_raw * -1);
 
   }
 }
